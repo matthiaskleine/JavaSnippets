cf0f77e51f2e08077cfa7a9521be460eeacb3f53
mkleine <kleine_matthias@gmx.de>
2017-03-08 13:16:01 +0100

    Add an initialization possibility.

--- src/collections/ArrayToList.java
+++ src/collections/ArrayToList.java
@@ -14,14 +14,21 @@ public class ArrayToList {
 	
 	// Version 1
 	// Do it for arrays of some object type.
-	public List<Integer> mymethod1(Integer[] array) {
+	public static List<Integer> mymethod1(Integer[] array) {
 		return Arrays.asList(array);
+	}
+
+	// Version 1.1
+	// Initialize the collection by a literally given array.
+	// Boxing works here because of the varargs signature of asList(Integer... a).
+	public static List<Integer> mymethod1_1() {
+		return Arrays.asList(1, 2, 3, 4);
 	}
 
 	// Version 2
 	// Do it for arrays of primitive types.
 	// Arrays.asList won't work here, because it does not deal with boxing.
-	public List<Integer> mymethod2(int[] array) {
+	public static List<Integer> mymethod2(int[] array) {
 		List<Integer> result = new ArrayList<Integer>();
 		for (int i : array) 
 			result.add(i);
@@ -30,7 +37,7 @@ public class ArrayToList {
 
 	// Version 3
 	// Do it for arrays of primitive types by using Google Guava:
-	public List<Integer> mymethod3(int[] array) {
+	public static List<Integer> mymethod3(int[] array) {
 		return Ints.asList(array);
 	}
 		
